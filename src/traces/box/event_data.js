668e3540f5233a3625b2b19d3d8294861aea59e1
radhey <radhey.varshney@yavda.in>
2023-09-04 16:26:05 +0530

    box plot related changes

--- src/traces/box/event_data.js
+++ src/traces/box/event_data.js
@@ -4,8 +4,14 @@ module.exports = function eventData(out, pt) {
     // Note: hoverOnBox property is needed for click-to-select
     // to ignore when a box was clicked. This is the reason box
     // implements this custom eventData function.
-    if(pt.hoverOnBox) out.hoverOnBox = pt.hoverOnBox;
-
+    if (pt.hoverOnBox) {
+        out.hoverOnBox = pt.hoverOnBox;
+        out.pointIndices = pt.cd[pt.index].pts.map(p=>p.i);
+        out.customdata = pt.cd[pt.index].trace.customdata[out.pointIndices[0]];
+        out.attr = pt.attr;
+        out.attrVal = pt.yVal;
+    }
+          
     if('xVal' in pt) out.x = pt.xVal;
     if('yVal' in pt) out.y = pt.yVal;
     if(pt.xa) out.xaxis = pt.xa;
