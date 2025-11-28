d33bac766ebe58af629cfc95148fc56619c77c06
radhey <radhey.varshney@yavda.in>
2025-11-28 13:21:28 +0530

    removed lint errors.

--- src/plots/cartesian/graph_interact.js
+++ src/plots/cartesian/graph_interact.js
@@ -152,7 +152,7 @@ exports.initInteractions = function initInteractions(gd) {
 // should trigger a full initInteractions.
 exports.updateFx = function(gd) {
     var fullLayout = gd._fullLayout;
+    // var cursor = fullLayout.dragmode === 'pan' ? 'move' : 'crosshair';
     var cursor = fullLayout.dragmode === 'zoom' ? 'cross-hair' : 'default';
-    //var cursor = fullLayout.dragmode === 'pan' ? 'move' : 'crosshair';
     setCursor(fullLayout._draggers, cursor);
 };
